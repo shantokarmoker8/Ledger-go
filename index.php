5dd0cfc96de7c37a5eaf6e7125ec2e10dba451c7
shantokarmoker8 <shantokarmoker8@gmail.com>
2026-07-15 21:06:12 +0600

    delete error

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $currentLang = $_SESSION['language'] ?? 'en';
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Cash Khata - <?php echo lang('dashboard'); ?></title>
+<title>Cash Khata</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['user_id'])) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Cash Khata - Login</title>
+<title>Cash Khata</title>
 <link rel="shortcut icon" href="./assets/logo.png" type="image/x-icon">
 <!-- Bootstrap 5 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
